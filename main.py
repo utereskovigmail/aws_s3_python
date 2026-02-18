e9b9a7dbcf896afb404e431b00dbb0d588abb4ed
George <utereskovygmail.com@MacBook-Pro.local>
2026-02-18 20:42:55 +0200

    test upload to s3

--- main.py
+++ main.py
@@ -2,25 +2,26 @@ import os
 import boto3
 import threading
 
-#print("Сало - це смачно і дуже корисно!")
-# s3_client = boto3.client('s3') # S3 client
-# s3_resource = boto3.resource('s3') # S3 resource
 
-# response = s3_client.list_buckets() # List all buckets
-# for bucket in response['Buckets']:
-#     print(bucket)
+s3_client = boto3.client('s3') # S3 client
+s3_resource = boto3.resource('s3') # S3 resource
+
+response = s3_client.list_buckets() # List all buckets
+for bucket in response['Buckets']:
+    print(bucket)
 
 # response = s3_client.list_objects_v2(Bucket='demo-bucket-bober') # List objects in a bucket
 # objects = response.get('Contents', [])
 # print(objects)
 
-# # s3_client.download_file("demo-bucket-bober", "bober.jpg", "downloaded_bober.jpg") 
-# # s3_client.download_file("demo-bucket-bober", "info.txt", "downloaded_info.txt") 
+# s3_client.download_file("demo-bucket-bober", "bober.jpg", "downloaded_bober.jpg") 
+# s3_client.download_file("demo-bucket-bober", "info.txt", "downloaded_info.txt") 
 
-# s3_client.put_bucket_versioning(
-#     Bucket='demo-bucket-bober', 
-#     VersioningConfiguration={'Status': 'Enabled'}
-# ) # Enable versioning on a bucket
+bucket_name = "demo-bucket-bober"
+s3_client.put_bucket_versioning(
+    Bucket=bucket_name, 
+    VersioningConfiguration={'Status': 'Enabled'}
+) # Enable versioning on a bucket
 
 # response = s3_client.list_object_versions(
 #     Bucket='demo-bucket-bober',
@@ -52,5 +53,15 @@ def backup_postgres():
         subprocess.run(command, stdout=f)
 
     print(f"Backup saved to {filename}")
+
+    s3_client.upload_file(
+        filename,   # local file
+        bucket_name,           # bucket name
+        f"backups/{filename}"  # key (path inside bucket)
+    )
+
+    print("Upload to s3 complete")
+
+
 
 backup_postgres()
