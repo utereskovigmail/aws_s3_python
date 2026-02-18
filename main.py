2b1bbcf48e9b789aaa9f58237506d6f70a631639
George <utereskovygmail.com@MacBook-Pro.local>
2026-02-18 20:36:57 +0200

    back up test

--- main.py
+++ main.py
@@ -1,30 +1,56 @@
 import os
 import boto3
+import threading
 
 #print("Сало - це смачно і дуже корисно!")
-s3_client = boto3.client('s3') # S3 client
-s3_resource = boto3.resource('s3') # S3 resource
+# s3_client = boto3.client('s3') # S3 client
+# s3_resource = boto3.resource('s3') # S3 resource
 
-response = s3_client.list_buckets() # List all buckets
-for bucket in response['Buckets']:
-    print(bucket)
+# response = s3_client.list_buckets() # List all buckets
+# for bucket in response['Buckets']:
+#     print(bucket)
 
-response = s3_client.list_objects_v2(Bucket='demo-bucket-bober') # List objects in a bucket
-objects = response.get('Contents', [])
-print(objects)
+# response = s3_client.list_objects_v2(Bucket='demo-bucket-bober') # List objects in a bucket
+# objects = response.get('Contents', [])
+# print(objects)
 
-# s3_client.download_file("demo-bucket-bober", "bober.jpg", "downloaded_bober.jpg") 
-# s3_client.download_file("demo-bucket-bober", "info.txt", "downloaded_info.txt") 
+# # s3_client.download_file("demo-bucket-bober", "bober.jpg", "downloaded_bober.jpg") 
+# # s3_client.download_file("demo-bucket-bober", "info.txt", "downloaded_info.txt") 
 
-s3_client.put_bucket_versioning(
-    Bucket='demo-bucket-bober', 
-    VersioningConfiguration={'Status': 'Enabled'}
-) # Enable versioning on a bucket
+# s3_client.put_bucket_versioning(
+#     Bucket='demo-bucket-bober', 
+#     VersioningConfiguration={'Status': 'Enabled'}
+# ) # Enable versioning on a bucket
 
-response = s3_client.list_object_versions(
-    Bucket='demo-bucket-bober',
-    Prefix='info.txt'
-) # List object versions in a bucket
+# response = s3_client.list_object_versions(
+#     Bucket='demo-bucket-bober',
+#     Prefix='info.txt'
+# ) # List object versions in a bucket
 
-for version in response.get('Versions'):
-    print(version)
+# for version in response.get('Versions'):
+#     print(version)
+
+import subprocess
+import datetime
+
+def backup_postgres():
+    timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+    filename = f"backup_{timestamp}.sql"
+
+    password = os.environ.get("DB_PASSWORD")
+    
+    command = [
+        "docker", "exec",
+        "-e", f"PGPASSWORD={password}",
+        "postgres_db",
+        "pg_dump",
+        "-U", "postgres",
+        "mydb"
+    ]
+
+    with open(filename, "w") as f:
+        subprocess.run(command, stdout=f)
+
+    print(f"Backup saved to {filename}")
+
+backup_postgres()
